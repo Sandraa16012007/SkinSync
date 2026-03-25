a4f052418b459c594f4c3278f16e596d615cbcc9
Aryan Das <aryandas2911@gmail.com>
2026-03-25 22:04:07 +0530

    Ingredient parser function impletemented

--- src/utils/ingredientParser.js
+++ src/utils/ingredientParser.js
@@ -1,1 +1,10 @@
-export const parseIngredients = () => {};
+export const parseIngredients = (rawText) => {
+  if (!rawText || typeof rawText !== 'string') return [];
+
+  return rawText
+    .toLowerCase()
+    .replace(/^ingredients\s*:\s*/, '') // Remove "ingredients:" prefix
+    .split(',')
+    .map(item => item.trim())
+    .filter(item => item.length > 0);
+};
